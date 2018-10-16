dbff1504a111896d56a33c4895ed40977dcc4308
IsmarySalgesM <ismarysalges@gmail.com>
2018-10-16 13:10:18 -0300

    Updates

---
+++ precache-manifest.70f5ae0633ff4d8c7d1a69473873bbb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46af5ef5d5ad9a312a93",
+    "url": "/IsmarySalgesM/MovieApi/static/js/runtime~main.46af5ef5.js"
+  },
+  {
+    "revision": "056fc46d11492104ae8d",
+    "url": "/IsmarySalgesM/MovieApi/static/js/main.056fc46d.chunk.js"
+  },
+  {
+    "revision": "00cae608af71f2284d55",
+    "url": "/IsmarySalgesM/MovieApi/static/js/1.00cae608.chunk.js"
+  },
+  {
+    "revision": "056fc46d11492104ae8d",
+    "url": "/IsmarySalgesM/MovieApi/static/css/main.a659643e.chunk.css"
+  },
+  {
+    "revision": "00cae608af71f2284d55",
+    "url": "/IsmarySalgesM/MovieApi/static/css/1.4884bad6.chunk.css"
+  },
+  {
+    "revision": "7b448251e3d480739f98acaf80a0d196",
+    "url": "/IsmarySalgesM/MovieApi/index.html"
+  }
+];
